65f570684602b2f4f704cf8f15d7cf446f40d750
Christian Ellis <ca_ls@yahoo.com>
2022-07-07 11:39:04 -0700

    commit tree e4249c77022d6a79da06b4811ea832e7c365b6d2

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b){
+return (a+b);
+};
+
+function subtract(a,b){
+return (a-b);
+}
+function multiply(a,b){
+return (a*b);
+}
+function divide(a,b){
+return (a/b);
+}
+function increment(a){
+    return (++a);
+}
+function decrement(a){
+    return (--a);
+}
+function makeInt(n){
+    parseInt(n, 10);
+    return((n));
+}
+makeInt("0*2328", 10)
